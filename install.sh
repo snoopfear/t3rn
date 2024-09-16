9d9441c12ad11f3820243f9672aebb12d5ee34f1
snoopfear <39308472+snoopfear@users.noreply.github.com>
2024-09-17 00:30:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -46,11 +46,6 @@ export ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,l1rn'
 echo "Enabled Networks set to: $ENABLED_NETWORKS"
 echo
 
-#export EXECUTOR_ARBITRUM_SEPOLIA_RPC_URLS='https://arb-sepolia.g.alchemy.com/v2/lNTUkf8We6CAI3gpMaBGyPsSz7bRk2U1'
-#export EXECUTOR_OPTIMISM_SEPOLIA_RPC_URLS='https://opt-sepolia.g.alchemy.com/v2/lNTUkf8We6CAI3gpMaBGyPsSz7bRk2U1'
-#export EXECUTOR_BASE_SEPOLIA_RPC_URLS='https://base-sepolia.g.alchemy.com/v2/lNTUkf8We6CAI3gpMaBGyPsSz7bRk2U1'
-#export EXECUTOR_BLAST_SEPOLIA_RPC_URLS='https://blast-sepolia.g.alchemy.com/v2/lNTUkf8We6CAI3gpMaBGyPsSz7bRk2U1'
-
 echo "Starting the Executor..."
 ./executor
 
