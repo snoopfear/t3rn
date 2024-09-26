e2cb4e85ef2faa2f5df3adf9711653a4094f9df6
snoopfear <39308472+snoopfear@users.noreply.github.com>
2024-09-26 15:24:26 +0300

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -76,10 +76,10 @@ Environment="LOG_LEVEL=debug"
 Environment="LOG_PRETTY=false"
 Environment="PRIVATE_KEY_LOCAL=$PRIVATE_KEY_LOCAL"
 Environment="ENABLED_NETWORKS=arbitrum-sepolia,optimism-sepolia,l1rn"
-Environment="RPC_ENDPOINTS_ARBT=https://rpc.ankr.com/arbitrum_sepolia/d52e9aed12d3e5eb1570d715d18bba7f60fdc942dd48df6e494e6bc00844e37c"
-Environment="RPC_ENDPOINTS_OPSP=https://rpc.ankr.com/optimism_sepolia/d52e9aed12d3e5eb1570d715d18bba7f60fdc942dd48df6e494e6bc00844e37c"
-Environment="RPC_ENDPOINTS_BSSP=https://rpc.ankr.com/base_sepolia/d52e9aed12d3e5eb1570d715d18bba7f60fdc942dd48df6e494e6bc00844e37c"
-Environment="RPC_ENDPOINTS_BLSS=https://rpc.ankr.com/blast_testnet_sepolia/d52e9aed12d3e5eb1570d715d18bba7f60fdc942dd48df6e494e6bc00844e37c"
+Environment="RPC_ENDPOINTS_ARBT=https://rpc.ankr.com/arbitrum_sepolia/3f5b31bf26186c6e7a8cb38c86d1c876cfb0c3f300a26f86c0faab3bef50d2f2"
+Environment="RPC_ENDPOINTS_OPSP=https://rpc.ankr.com/optimism_sepolia/3f5b31bf26186c6e7a8cb38c86d1c876cfb0c3f300a26f86c0faab3bef50d2f2"
+Environment="RPC_ENDPOINTS_BSSP=https://rpc.ankr.com/base_sepolia/3f5b31bf26186c6e7a8cb38c86d1c876cfb0c3f300a26f86c0faab3bef50d2f2"
+Environment="RPC_ENDPOINTS_BLSS=https://rpc.ankr.com/blast_testnet_sepolia/3f5b31bf26186c6e7a8cb38c86d1c876cfb0c3f300a26f86c0faab3bef50d2f2"
 
 #,base-sepolia,blast-sepolia
 
