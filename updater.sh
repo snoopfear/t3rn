37898daad9d329a61833efcba133b94a49513d98
snoopfear <39308472+snoopfear@users.noreply.github.com>
2024-12-04 00:35:27 +0300

    Create updater.sh

---
+++ updater.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo "Welcome to the t3rn Executor Updater by snoopfear!"
+
+# Проверка на существование переменной PRIVATE_KEY_LOCAL
+if [ -z "${PRIVATE_KEY_LOCAL}" ]; then
+    # Запрос ввода приватного ключа
+    read -p "Enter your Private Key from Metamask: " PRIVATE_KEY_LOCAL
+    # Экспорт переменной
+    export PRIVATE_KEY_LOCAL
+    
+    # Сохранение переменной в .bashrc для постоянного использования
+    echo "export PRIVATE_KEY_LOCAL=\"$PRIVATE_KEY_LOCAL\"" >> ~/.bashrc
+else
+    echo "Using existing PRIVATE_KEY_LOCAL: $PRIVATE_KEY_LOCAL"
+fi
+
+# Переход в домашнюю директорию
+cd $HOME
+sudo systemctl start executor
+
+# Удаление архива после распаковки
+rm -rf $EXECUTOR_FILE
+
+# Удаление старой папки с executor'ом
+rm -rf ~/executor
+
+# Обновление и улучшение пакетов
+sudo apt -qy upgrade
+
+# Указание ссылки для загрузки и имени файла
+EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.26.0/executor-linux-v0.26.0.tar.gz"
+EXECUTOR_FILE="executor-linux-v0.26.0.tar.gz"
+
+echo "Downloading the Executor binary from $EXECUTOR_URL..."
+curl -L -o $EXECUTOR_FILE $EXECUTOR_URL
+
+# Проверка успешности загрузки
+if [ $? -ne 0 ]; then
+    echo "Failed to download the Executor binary. Please check your internet connection and try again."
+    exit 1
+fi
+
+echo "Extracting the binary..."
+tar -xzvf $EXECUTOR_FILE
+
+# Удаление архива после распаковки
+rm -rf $EXECUTOR_FILE
+
+sudo systemctl restart executor"
+journalctl -n 100 -f -u executor | ccze -A
