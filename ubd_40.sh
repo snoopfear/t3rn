6ad122203a928eea8f3a05d2c8b5e429ba639eab
snoopfear <39308472+snoopfear@users.noreply.github.com>
2025-01-19 23:39:04 +0300

    Update and rename ubd_38.sh to ubd_40.sh

--- ubd_40.sh
+++ ubd_40.sh
@@ -35,8 +35,8 @@ echo "Updating and upgrading system packages..."
 sudo apt update && sudo apt -qy upgrade
 
 # Шаг 5: Указание ссылки для загрузки и имени файла
-EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.38.0/executor-linux-v0.38.0.tar.gz"
-EXECUTOR_FILE="executor-linux-v0.38.0.tar.gz"
+EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.40.0/executor-linux-v0.40.0.tar.gz"
+EXECUTOR_FILE="executor-linux-v0.40.0.tar.gz"
 
 echo "Downloading the Executor binary from $EXECUTOR_URL..."
 curl -L -o $EXECUTOR_FILE $EXECUTOR_URL
