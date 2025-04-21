a58a5e86e6309249b282cb2b25514d26f666b5bf
snoopfear <39308472+snoopfear@users.noreply.github.com>
2025-04-21 13:45:56 +0300

    Update upd_service.sh

--- upd_service.sh
+++ upd_service.sh
@@ -28,11 +28,11 @@ Environment="EXECUTOR_PROCESS_CLAIMS_API_ENABLED=false"
 Environment="NETWORKS_DISABLED=blast-sepolia,monad-testnet"
 Environment="ENABLED_NETWORKS=arbitrum-sepolia,optimism-sepolia,l1rn,base-sepolia,unichain-sepolia"
 Environment="RPC_ENDPOINTS={\
-    \"l2rn\": [\"https://t3rn-b2n.blockpi.network/v1/rpc/public\", \"https://b2n.rpc.caldera.xyz/http\"],\
-    \"arbt\": [\"https://arb-sepolia.g.alchemy.com/v2/$APIKEY"],\
-    \"bast\": [\"https://base-sepolia.g.alchemy.com/v2/$APIKEY"],\
-    \"opst\": [\"https://opt-sepolia.g.alchemy.com/v2/$APIKEY"],\
-    \"unit\": [\"https://unichain-sepolia.g.alchemy.com/v2/$APIKEY"]\
+    \\"l2rn\\": [\\"https://t3rn-b2n.blockpi.network/v1/rpc/public\\", \\"https://b2n.rpc.caldera.xyz/http\\"],\
+    \\"arbt\\": [\\"https://arb-sepolia.g.alchemy.com/v2/$APIKEY\\"],\
+    \\"bast\\": [\\"https://base-sepolia.g.alchemy.com/v2/$APIKEY\\"],\
+    \\"opst\\": [\\"https://opt-sepolia.g.alchemy.com/v2/$APIKEY\\"],\
+    \\"unit\\": [\\"https://unichain-sepolia.g.alchemy.com/v2/$APIKEY\\"]\
 }"
 Restart=always
 RestartSec=5
