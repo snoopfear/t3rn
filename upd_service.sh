e2683d8ca7f34d2d534143c770a3c48bc489aed2
snoopfear <39308472+snoopfear@users.noreply.github.com>
2025-04-21 13:40:50 +0300

    Update upd_service.sh

--- upd_service.sh
+++ upd_service.sh
@@ -1,13 +1,15 @@
-read -p "Enter your API Key: " APIKEY && \
-sed -i '/APIKEY=/d' ~/.bashrc && \
-echo "export APIKEY=\"$APIKEY\"" >> ~/.bashrc && \
-sudo systemctl stop executor && \
-sudo systemctl disable executor && \
-sudo rm -f /etc/systemd/system/executor.service && \
+read -p "Enter your API Key: " APIKEY
+sed -i '/APIKEY=/d' ~/.bashrc
+echo "export APIKEY=\"$APIKEY\"" >> ~/.bashrc
+sudo systemctl stop executor
+sudo systemctl disable executor
+sudo rm -f /etc/systemd/system/executor.service
+
 cat <<EOF | sudo tee /etc/systemd/system/executor.service >/dev/null
 [Unit]
 Description=t3rn Executor Service
 After=network.target
+
 [Service]
 ExecStart=$HOME/executor/executor/bin/executor
 Environment="ENVIRONMENT=testnet"
@@ -26,11 +28,11 @@ Environment="EXECUTOR_PROCESS_CLAIMS_API_ENABLED=false"
 Environment="NETWORKS_DISABLED=blast-sepolia,monad-testnet"
 Environment="ENABLED_NETWORKS=arbitrum-sepolia,optimism-sepolia,l1rn,base-sepolia,unichain-sepolia"
 Environment="RPC_ENDPOINTS={\
-    \\\"l2rn\\\": [\\\"https://t3rn-b2n.blockpi.network/v1/rpc/public\\\", \\\"https://b2n.rpc.caldera.xyz/http\\\"],\
-    \\\"arbt\\\": [\\\"https://arb-sepolia.g.alchemy.com/v2/$APIKEY\\"],\
-    \\\"bast\\\": [\\\"https://base-sepolia.g.alchemy.com/v2/$APIKEY\\"],\
-    \\\"opst\\\": [\\\"https://opt-sepolia.g.alchemy.com/v2/$APIKEY\\"],\
-    \\\"unit\\\": [\\\"https://unichain-sepolia.g.alchemy.com/v2/$APIKEY\\"]\
+    \"l2rn\": [\"https://t3rn-b2n.blockpi.network/v1/rpc/public\", \"https://b2n.rpc.caldera.xyz/http\"],\
+    \"arbt\": [\"https://arb-sepolia.g.alchemy.com/v2/$APIKEY"],\
+    \"bast\": [\"https://base-sepolia.g.alchemy.com/v2/$APIKEY"],\
+    \"opst\": [\"https://opt-sepolia.g.alchemy.com/v2/$APIKEY"],\
+    \"unit\": [\"https://unichain-sepolia.g.alchemy.com/v2/$APIKEY"]\
 }"
 Restart=always
 RestartSec=5
@@ -38,4 +40,9 @@ RestartSec=5
 [Install]
 WantedBy=multi-user.target
 EOF
-&& sudo systemctl daemon-reexec && sudo systemctl daemon-reload && sudo systemctl enable executor && sudo systemctl restart executor && journalctl -n 100 -f -u executor | ccze -A
+
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+sudo systemctl enable executor
+sudo systemctl restart executor
+journalctl -n 100 -f -u executor | ccze -A
